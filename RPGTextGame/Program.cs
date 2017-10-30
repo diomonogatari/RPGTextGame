6ae64e4bd9f7f58ae83cda1b1b0fb8e37547b209
Diogo Carvalho <diogo@waterdog.mobi>
2017-10-30 21:19:58 +0000

    Initial commit

--- RPGTextGame/Program.cs
+++ RPGTextGame/Program.cs
@@ -10,6 +10,14 @@ namespace RPGTextGame
     {
         static void Main(string[] args)
         {
+            introText();
+            Console.Read();
+        }
+
+
+        private static void introText()
+        {
+            Console.WriteLine("Greetings cunt \nyou suck dick");
         }
     }
 }
